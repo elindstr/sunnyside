d7bfa622d214dfaef918d1b476fd4bf100c7d995
elindstr <elindstr@gmail.com>
2024-04-03 21:42:56 -0700

    adding logo to nodemailer emails

--- controllers/admin/emailRoutes.js
+++ controllers/admin/emailRoutes.js
@@ -52,7 +52,8 @@ router.post('/credentials/:id', withAdminAuth, async (req, res) => {
           <li>password: ${newPassword}</li>
       </ul><br>
       
-      <img src='https://sunnyside-699326087e54.herokuapp.com/assets/logo.png' width='190px' alt='Sunnyside Pools'>`,
+      <img src='https://sunnyside-699326087e54.herokuapp.com/assets/logo.png' width='190px' alt='Sunnyside Pools'>
+      `,
     }
     
     // send email
